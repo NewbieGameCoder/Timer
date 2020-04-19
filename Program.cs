ab2ad1f322959949e7ec384af6ecebb3f66f0f85
NewbieGameCoder <596809147@qq.com>
2020-04-19 23:35:40 +0800

    完善测试用例

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Timer
 {
@@ -8,14 +9,26 @@ namespace Timer
         {
             int icount = 0;
             TimeSpan ts = DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            Dictionary<int, Action> dict = new Dictionary<int, Action>();
+            List<int> list = new List<int>();
+
             for (int i = 0; i < 150; ++i)
             {
                 int index = i;
-                ETimer.Timer.Instance.Add((long)ts.TotalSeconds + i, () =>
+                Action trigger = () =>
                 {
                     Console.WriteLine(index);
                     Console.WriteLine(icount++);
-                });
+                };
+                int timerID = ETimer.Timer.Instance.Add((long)ts.TotalSeconds + i, trigger);
+                list.Add(timerID);
+                dict.Add(timerID, trigger);
+            }
+
+            for (int i = 1; i < 150; i = i + 2)
+            {
+                int timerID = list[i];
+                ETimer.Timer.Instance.Remove(timerID, dict[timerID]);
             }
 
             while (icount < 150)
